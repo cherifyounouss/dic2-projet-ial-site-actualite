c5a99921c55d5c7a9abb7a672c85d8f91577cb8e
cherifyounouss <chinsanity16@gmail.com>
2020-10-28 02:19:02 +0000

    add get user method

--- web-services/soap-services/src/users/UserDAO.java
+++ web-services/soap-services/src/users/UserDAO.java
@@ -30,6 +30,40 @@ public class UserDAO {
             user.setId((int)generatedKeys.getLong(1));
         else
             throw new SQLException("Creating user failed ! No id retrieved");
+        statement.close();
+        conn.close();
+        return user;
+    }
+
+    public User getUser(int id) throws SQLException {
+        Connection conn = ConnectionManager.getConnection();
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = conn.prepareStatement("SELECT * FROM user WHERE id_user = ?");
+            statement.setInt(1, id);
+            if (statement.execute()) {
+                resultSet = statement.getResultSet();
+                if (resultSet != null && resultSet.next())
+                    return getUser(resultSet);
+            }
+        }
+        finally {
+            if (statement != null)
+                statement.close();
+            if (resultSet != null)
+                resultSet.close();
+            conn.close();
+        }
+        return null;
+    }
+
+    private User getUser(ResultSet resultSet) throws SQLException {
+        User user = new User();
+        user.setId(resultSet.getInt("id_user"));
+        user.setName(resultSet.getString("name"));
+        user.setLogin(resultSet.getString("login"));
+        user.setPassword("");
         return user;
     }
 }
