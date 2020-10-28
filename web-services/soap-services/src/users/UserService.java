63a16a2e572a8279fbaebd7439511dd87d370888
cherifyounouss <chinsanity16@gmail.com>
2020-10-28 02:29:30 +0000

    add get user service

--- web-services/soap-services/src/users/UserService.java
+++ web-services/soap-services/src/users/UserService.java
@@ -20,4 +20,9 @@ public class UserService {
     public User create(@WebParam(name = "name") String name, @WebParam(name = "login") String login, @WebParam(name = "password") String password) throws SQLException{
         return new UserDAO().createUser(new User(name, login, password));
     }
+
+    @WebMethod
+    public User get(@WebParam(name = "id") int id) throws SQLException{
+        return new UserDAO().getUser(id);
+    }
 }
