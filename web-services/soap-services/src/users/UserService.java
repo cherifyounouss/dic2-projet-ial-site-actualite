674689ea628cf660ce83fb3480f3f85b063ebc0e
cherifyounouss <chinsanity16@gmail.com>
2020-10-28 01:48:23 +0000

    adding create user service

---
+++ web-services/soap-services/src/users/UserService.java
@@ -0,0 +1,23 @@
+package users;
+
+import java.sql.SQLException;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+/**
+ * UserService
+ */
+
+@WebService
+public class UserService {
+
+    
+    public UserService() {}
+
+    @WebMethod
+    public User create(@WebParam(name = "name") String name, @WebParam(name = "login") String login, @WebParam(name = "password") String password) throws SQLException{
+        return new UserDAO().createUser(new User(name, login, password));
+    }
+}
