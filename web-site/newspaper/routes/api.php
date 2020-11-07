ca18e9f1a5bcc7b13a61ae59994a5733b4b10839
Pape Idrissa NDOYE <ndoyeidrissa530@gmail.com>
2020-11-07 04:46:02 +0000

    found a way to use the same methods either for the intern app or for APIs

--- web-site/newspaper/routes/api.php
+++ web-site/newspaper/routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('articles', 'ArticleController@get_articles');
+
+Route::get('articles/{id}', 'ArticleController@get_article');
+
+Route::get('articles/category/{category_id}', 'ArticleController@get_articles_by_category');
