e81d1a4030346076d42bf067c5977866c9c6b6d8
cherifyounouss <chinsanity16@gmail.com>
2020-11-12 14:54:01 +0000

    Token generation and verification

--- web-services/soap-services/src/users/UserService.java
+++ web-services/soap-services/src/users/UserService.java
@@ -1,10 +1,17 @@
 package users;
 
 import java.sql.SQLException;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
+import javax.crypto.SecretKey;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import javax.xml.bind.DatatypeConverter;
+
+import security.Symmetric;
 
 /**
  * UserService
@@ -13,8 +20,13 @@ import javax.jws.WebService;
 @WebService
 public class UserService {
 
+    SecretKey secretKey;
+    byte[] initializationVector;
     
-    public UserService() {}
+    public UserService() {
+        this.initializationVector = Symmetric.createInitializeVector();
+        this.secretKey = Symmetric.createAESKey();
+    }
 
     @WebMethod
     public User create(@WebParam(name = "name") String name, @WebParam(name = "login") String login, @WebParam(name = "password") String password) throws SQLException{
@@ -24,5 +36,65 @@ public class UserService {
     @WebMethod
     public User get(@WebParam(name = "id") int id) throws SQLException{
         return new UserDAO().getUser(id);
+    }
+
+    @WebMethod
+    public List<User> getAll(@WebParam(name = "token", header = true) String token) throws SQLException, InvalidTokenException {
+        if (token == null) {
+            System.out.println("inside block 1");
+            throw new InvalidTokenException("Please submit a valid token for accessing this ressource");
+        }
+        boolean isTokenValid = verifyToken(token);
+        if (!isTokenValid)
+            throw new InvalidTokenException("Please submit a valid token for accessing this ressource");
+        return new UserDAO().getAllUsers();
+    }
+
+    @WebMethod
+    public boolean delete(@WebParam(name = "id") int id) throws SQLException{
+        return new UserDAO().deleteUser(id);
+    }
+
+    @WebMethod
+    public boolean update(@WebParam(name = "id") int id, @WebParam(name = "name") String name, @WebParam(name = "login") String login, @WebParam(name = "password") String password) throws SQLException {
+        return new UserDAO().updateUser(new User(id, name, login, password));
+    }
+
+    @WebMethod
+    public String authenticate(@WebParam(name = "login") String login, @WebParam(name = "password") String password) throws AuthenticationFailed, SQLException{
+        User user = new UserDAO().authenticate(login, password);
+        if (user == null)
+            throw new AuthenticationFailed("Authentication failed");
+        return getToken(user);
+    }
+
+    private String getToken(User user) {
+        long expiryDate = System.currentTimeMillis() + 1800000;
+        String pText = user + "," + expiryDate;
+        byte[] token = Symmetric.doAESEncryption(pText, secretKey, initializationVector);
+        return DatatypeConverter.printHexBinary(token);
+    }
+
+    private boolean verifyToken(String token) {
+        System.out.println(token);
+        byte[] cipher = DatatypeConverter.parseHexBinary(token);
+        String pText = Symmetric.doAESDecryption(cipher, secretKey, initializationVector);
+        System.out.println(pText);
+        Scanner input = new Scanner(pText);
+        input.useDelimiter(",");
+        try {
+            User user = new User(input.nextInt(), input.next(), input.next());
+            long expiryTime = input.nextLong();
+            if ((expiryTime - System.currentTimeMillis()) < 0)
+                return false;
+            User userFromBase = new UserDAO().getUser(user.getId());
+            if (userFromBase != null && user.getLogin().equals(userFromBase.getLogin()))
+                return true;
+            else
+                return false;
+        }
+        catch (NoSuchElementException | SQLException e) {
+            return false;
+        }
     }
 }
